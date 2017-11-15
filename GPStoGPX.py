5bd9572cd51d894b358a5484f29bf7e871b77826
kcwfan <karencwfan@gmail.com>
2017-11-15 16:26:41 -0500

    minor fix of txt to gpx

--- GPStoGPX.py
+++ GPStoGPX.py
@@ -73,7 +73,7 @@ def parseGPS(GPSLine):
 	return GPSString
 
 def writeToDisk(GPSString):
-	file = open("testfile.txt","w")
+	file = open("testfile.gpx","w")
 	file.write(GPSString)
 	file.close()
 
