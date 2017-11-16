b900e977aacb8ca1bbd9d8e6aae12577df535fac
James VR <j.v.roggeveen@gmail.com>
2017-11-16 01:57:54 -0500

    Firmware Dev
    Attempts to create workable SX radio modules - still needs more work before it is fully functional.

--- hive_dummy_data_sender.py
+++ hive_dummy_data_sender.py
@@ -10,14 +10,16 @@ Created on Wed Nov 15 19:41:46 2017
 import serial
 
 packetqueue = [
-    bytearray([0x7e,17,0x03,0x00]+[0xaa]*15+[0x0]*64),
-    bytearray([0x7e,16,0x03,0x01]+[0xaa]*14+[0x0]*65),
-    bytearray([0x7e,1,0x03]+[0x00]*80)
+    bytearray([0x7e,0x17,0x02,0x00]+[0xaa]*15+[0x0]*64),
+    bytearray([0x7e,16,0x02,0x01]+[0xaa]*14+[0x0]*65),
+    bytearray([0x7e,1,0x02]+[0x00]*80)
 ]
 
-port = serial.Serial('COM4')
+port = serial.Serial('COM9')
 while packetqueue != []:
     packet = port.read(83)
+    print(packet.hex())
     if packet[2] == 0x02:
         port.write(packetqueue.pop(0))
+        print('Sending new packet')
 port.close()
