f0466ad8a911208c079945f24dba5cbc4b97e303
jctags <jctaglic@gmail.com>
2017-11-16 14:27:55 -0500

    syntax fixing

--- packetbuilder.py
+++ packetbuilder.py
@@ -14,14 +14,14 @@ def packet_from_hex_string(pstring):
         outstring = outstring + '0x'+substring+', '
     outstring = outstring[0:-2] + '};'
     return outstring
- 
+
 def fill_hex_string(string):
     return string + '0'*(166-len(string))
-    
+
 def button_press_packet(button):
     string = '7e02010' + str(button)
     return packet_from_hex_string(fill_hex_string(string))
-    
+
 def joystick_packet(direction):
     if direction == 'up':
         varstring = '01ff0100'
@@ -33,7 +33,7 @@ def joystick_packet(direction):
         varstring == '010002ff'
     string = '7e060106'+varstring
     return packet_from_hex_string(fill_hex_string(string))
-    
+
 def gps_scout_packet(lat,long,scoutnum,poi=False):
     if poi:
         poi = '1'
@@ -59,7 +59,7 @@ def number_to_packet_hex(number):
 
 def hive_data_request_packet():
     return packet_from_hex_string(fill_hex_string('7e0102'))
-    
+
 def packets_to_c(packets):
     string = 'void setup() {Serial.begin(9600);} '
     string = string + ' void loop() { '
@@ -67,12 +67,19 @@ def packets_to_c(packets):
         string =string+' { '+packet+' for(int i = 0; i < 83; i++) {Serial.write(packet[i]);} delay(5000); } '
     string = string + ' } '
     return string
-        
+
 #packet_from_hex_string('7e0d000b598602017cc43c040201010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000')
 #print(packet_from_hex_string(button_press_hex_string(0)))
 print(packets_to_c([
-    gps_scout_packet(42.359051,-71.091324,1,True),
-    button_press_packet(0),
-    joystick_packet('up'),
-    hive_data_request_packet()
+    gps_scout_packet(42.358340,-71.094600,0,False),
+    gps_scout_packet(42.358360,-71.094620,0,False),
+    gps_scout_packet(42.358380,-71.094640,0,False),
+    gps_scout_packet(42.358380,-71.094660,0,False),
+    gps_scout_packet(42.358380,-71.094680,0,False),
+    gps_scout_packet(42.358360,-71.094700,0,False),
+    gps_scout_packet(42.358340,-71.094700,0,False),
+    gps_scout_packet(42.358380,-71.094660,0,True),
+    #button_press_packet(0),
+    #joystick_packet('up'),
+    #hive_data_request_packet()
 ]))
