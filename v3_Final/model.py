6f345625709ce7a242163b16d680e416e220add8
jctags <jctaglic@gmail.com>
2017-11-29 01:31:43 -0500

    backend untested

---
+++ v3_Final/model.py
@@ -0,0 +1,64 @@
+import pymongo
+
+class DataPoint(object):
+    def init(self, scout, queen, is_poi, is_current, latitude, longitude, description, time, needs_transmit):
+        self.scout = scout
+        self.queen = queen
+        self.is_waypoint = is_poi
+        self.is_current = is_current
+        self.latitude = latitude
+        self.longitude = longitude
+        self.description = description
+        self.time = time
+        self.needs_transmit = needs_transmit
+
+    def mongo_dict(self):
+        return {
+            'scout': self.scout,
+            'queen': self.queen,
+            'time': self.time,
+            'longitude': self.longitude,
+            'latitude': self.latitude,
+            'isCurrent': self.is_current,
+            'isWaypoint': self.is_waypoint,
+            'needsTransmit': self.needs_transmit
+        }
+
+class Model(object):
+
+    def init(self):
+        self.client = pymongo.MongoClient('mongodb://localhost:27017/')
+        self.database = self.client.beacon #rename this based on the database
+        self.points = self.database.points
+
+    def add_location_data_point(self,scout,queen,slat,slon,time):
+        data = DataPoint(scout, queen, False, True, slat,slon,None,time,True)
+        self.points.update_many(
+            {'scout': data.scout, 'queen': data.queen, 'isCurrent':True },
+            {'isCurrent': False}
+        )
+        self.points.insert_one(data.mongo_dict())
+
+    def add_poi_data_point(self,scout,queen,plat,plon,time,description):
+        data = DataPoint(scout, queen, True, None, plat, plon, description, time, True)
+        self.points.insert_one(data.mongo_dict())
+
+    def add_hive_data_point(self, scout, queen, is_poi, is_current, latitude, longitude, description, time):
+        data = DataPoint(scout, queen, is_poi, is_current, latitude, longitude, description, time, None)
+        self.points.insert_one(data.mongo_dict())
+
+    def location_data_to_send(self):
+        data = []
+        ids = []
+        for point in self.points.find({'isWaypoint': False, 'needsTransmit': True}).limit(5):
+            data.append(point)
+            ids.append(point['_id'])
+        self.points.update_many({'_id':{'$in': ids}}, {'needsTransmit': False})
+        return output
+
+    def poi_data_to_send(self):
+        data = self.points.find_one({'isWaypoint': False,'needsTransmit': True})
+        if data is None:
+            return None
+        self.points.update_one({'_id': data['_id']},{'needsTransmit': False})
+        return data
